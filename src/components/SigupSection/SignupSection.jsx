c0951f767311a13d6bf7a85a0e81c6e5f6ad7abe
Rishav Kumar <rishavrajput2018@gmail.com>
2025-03-28 11:51:59 +0530

    signup

--- src/components/SigupSection/SignupSection.jsx
+++ src/components/SigupSection/SignupSection.jsx
@@ -44,6 +44,7 @@ const SignupSection = () => {
 
         {/* Instruction Text */}
         <p className="text-gray-600 mt-4">Please enter your 10-digit mobile number</p>
+        
 
         {/* Mobile Number Input */}
         <div className="mt-4 flex items-center border rounded-lg">
@@ -77,44 +78,7 @@ const SignupSection = () => {
     </div>
 
 
-    {/* --------------------old---------------------------- */}
-      {/* <div>
-      <div className="flex justify-center items-center h-1/2 md:h-screen bg-gray-100 gap-5">
-        <div>
-            <img src={woman} alt="woman-object-working" />
-        </div>
-      <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-lg">
-        <div className="flex justify-between items-center border-b pb-4">
-          <h2 className="text-xl font-semibold">Login/Signup with OTP</h2>
-          <button >
-          </button>
-        </div>
-
-        <p className="text-gray-600 mt-4">Please enter your 10 digit mobile number</p>
-        
-        <div className="mt-4 flex items-center border rounded-lg ">
-          <span className="px-4 py-2 bg-gray-200">🇮🇳 +91</span>
-          <input
-            type="tel"
-            placeholder="Enter your mobile number"
-            // value=""
-            // onChange=""
-            className="w-full p-2 outline-none"
-          />
-        </div>
-
-        <p className="text-sm text-gray-500 mt-2">
-          We will send a verification code via your phone number
-        </p>
-
-        <button
-          className="w-full mt-6 bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition"
-        >
-          Request OTP
-        </button>
-      </div>
-    </div>
-    </div> */}
+    
     </>
   )
 }
