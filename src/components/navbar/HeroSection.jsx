1d752346a3ba409285f66f6d8ab86319be6e4de8
Rishav Kumar <rishavrajput2018@gmail.com>
2025-04-04 18:30:31 +0530

    changes

--- src/components/navbar/HeroSection.jsx
+++ src/components/navbar/HeroSection.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+// import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+// import { IoArrowBackCircleOutline } from "react-icons/io5";
 import hero from "/OBJECTS.png";
 import ctg1 from "/ctg1.png";
 import ctg2 from "/ctg2.png";
@@ -178,9 +180,7 @@ const HeroSection = () => {
                   <img
                     src={book.image}
                     loading="lazy"
-
                     alt={book.title}
-
                     className="w-screen md:h-64 object-contain rounded-lg"
                   />
                   <h3 className="font-semibold text-gray-900 mt-4">
@@ -204,66 +204,62 @@ const HeroSection = () => {
           <div className="overflow-x-auto">
             {/* <marquee behavior=" " direction=""> */}
             <div className="grid grid-cols- xs:grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4 md:gap-6 items-center">
-                <div className="bg-white p-4 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300  sm:w-auto">
-                  <img
-                    src={ctg1}
-                    alt="Applied Science"
-                    loading="lazy"
-                    className="w-full h-40 object-cover rounded-md"
-                  />
-                  <h3 className="mt-3 text-lg font-semibold text-gray-700 text-center">
-                    Applied Science
-                  </h3>
-                </div>
-                <div className="bg-white p-4 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300  sm:w-auto">
-                  <img
-                    src={ctg2}
-                    alt="Computer Engineering"
-                    loading="lazy"
-
-                    className="w-full h-40 object-cover rounded-md"
-                  />
-                  <h3 className="mt-3 text-lg font-semibold text-gray-700 text-center">
-                    Computer Engineering
-                  </h3>
-                </div>
-                <div className="bg-white p-4 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300  sm:w-auto">
-                  <img
-                    src={ctg3}
-                    alt="Civil Engineering"
-                    loading="lazy"
-
-                    className="w-full h-40 object-cover rounded-md"
-                  />
-                  <h3 className="mt-3 text-lg font-semibold text-gray-700 text-center">
-                    Civil Engineering
-                  </h3>
-                </div>
-                <div className="bg-white p-4 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ">
-                  <img
-                    src={ctg4}
-                    alt="Mechanical Engineering"
-                    loading="lazy"
-
-                    className="w-full h-40 object-cover rounded-md"
-                  />
-                  <h3 className="mt-3 text-lg font-semibold text-gray-700 text-center">
-                    Mechanical Engineering
-                  </h3>
-                </div>
-                <div className="bg-white p-4 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300  sm:w-auto">
-                  <img
-                    src={ctg5}
-                    alt="Electrical Engineering"
-                    loading="lazy"
-
-                    className="w-full h-40 object-cover rounded-md"
-                  />
-                  <h3 className="mt-3 text-lg font-semibold text-gray-700 text-center">
-                    Electrical Engineering
-                  </h3>
-                </div>
+              <div className="bg-white p-4 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300  sm:w-auto">
+                <img
+                  src={ctg1}
+                  alt="Applied Science"
+                  loading="lazy"
+                  className="w-full h-40 object-cover rounded-md"
+                />
+                <h3 className="mt-3 text-lg font-semibold text-gray-700 text-center">
+                  Applied Science
+                </h3>
               </div>
+              <div className="bg-white p-4 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300  sm:w-auto">
+                <img
+                  src={ctg2}
+                  alt="Computer Engineering"
+                  loading="lazy"
+                  className="w-full h-40 object-cover rounded-md"
+                />
+                <h3 className="mt-3 text-lg font-semibold text-gray-700 text-center">
+                  Computer Engineering
+                </h3>
+              </div>
+              <div className="bg-white p-4 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300  sm:w-auto">
+                <img
+                  src={ctg3}
+                  alt="Civil Engineering"
+                  loading="lazy"
+                  className="w-full h-40 object-cover rounded-md"
+                />
+                <h3 className="mt-3 text-lg font-semibold text-gray-700 text-center">
+                  Civil Engineering
+                </h3>
+              </div>
+              <div className="bg-white p-4 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ">
+                <img
+                  src={ctg4}
+                  alt="Mechanical Engineering"
+                  loading="lazy"
+                  className="w-full h-40 object-cover rounded-md"
+                />
+                <h3 className="mt-3 text-lg font-semibold text-gray-700 text-center">
+                  Mechanical Engineering
+                </h3>
+              </div>
+              <div className="bg-white p-4 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300  sm:w-auto">
+                <img
+                  src={ctg5}
+                  alt="Electrical Engineering"
+                  loading="lazy"
+                  className="w-full h-40 object-cover rounded-md"
+                />
+                <h3 className="mt-3 text-lg font-semibold text-gray-700 text-center">
+                  Electrical Engineering
+                </h3>
+              </div>
+            </div>
             {/* </marquee> */}
           </div>
         </div>
@@ -272,7 +268,9 @@ const HeroSection = () => {
         <section className="bg-blue-900 text-white px-10  flex flex-col md:flex-row items-center justify-between rounded-lg shadow-lg mt-15 ">
           {/* Left Side : Text Content */}
           <div className="md:w-1/2 ">
-            <h3 className="text-xl font-semibold text-blue-100 underline">DOWNLOAD OUR APP</h3>
+            <h3 className="text-xl font-semibold text-blue-100 underline">
+              DOWNLOAD OUR APP
+            </h3>
             <h1 className="text-sm md:text-xl lg:text-4xl font-bold mt-2">
               Streamline your college studies with our app
             </h1>
@@ -314,83 +312,82 @@ const HeroSection = () => {
           </div>
         </section>
 
-
-       
-
-
-
         {/* ------------AuthorCarousel ---------------*/}
         <section className="bg-gray-100 py-12">
-          <div className="max-w-5xl mx-auto px-6">
+          <div className="">
             {/* Section Header */}
-            <h3 className="text-lg text-blue-900 font-semibold">AUTHORS</h3>
+            <h2 className="text-xl text-blue-900 font-semibold mb-2">
+              AUTHORS
+            </h2>
             <h2 className="text-3xl font-bold text-gray-900 mt-2">
               Creators of the Collection
             </h2>
 
             {/* Carousel Section */}
-            <div className="relative mt-8 ">
-              <div className="  grid md:grid lg:flex  gap-6  overflow-x-auto scrollbar-hide items-center">
-                <div className=" bg-white p-6 rounded-lg shadow-lg flex flex-col items-center w-80 min-w-[80%] md:min-w-[30%] transition-transform duration-300">
-                  <div className="flex items-center gap-10">
+            <div className="relative mt-10  ">
+              <div className="  grid  md:grid-cols-1 lg:grid-cols-3 gap-6 bg-gray-200 rounded-lg  shadow-lg p-10  ">
+                <div className=" bg-white p-6 rounded-lg shadow-lg flex flex-col  items-center  hover:scale-105 transition-transform duration-300">
+                  <div className="grid justify-items-center md:flex lg:flex text-center  gap-5 hover:scale-105 transition-transform duration-300">
                     <img
                       src={pic}
                       alt="Author- img"
                       loading="lazy"
                       className="w-16 h-16 rounded-full mb-4"
                     />
-                    <div>
+                    <div className="">
                       <h3 className="text-lg font-bold">James Anderson</h3>
 
-                      <p className="text-sm text-gray-500">UI/UX Designer</p>
+                      <p className="text-md text-gray-700 font-semibold">
+                        UI/UX Designer
+                      </p>
                     </div>
                   </div>
 
-                  <p className="text-gray-700 text-center mt-4">
+                  <p className="text-gray-900 text-center mt-4">
                     I recently purchased a dining table and chairs from this
                     store, and I couldn't be happier with the experience.
                   </p>
                 </div>
-                <div className=" bg-white p-6 rounded-lg shadow-lg flex flex-col items-center w-80 min-w-[80%] md:min-w-[30%] transition-transform duration-300">
-                  <div className="flex items-center gap-10">
+                <div className=" bg-white p-6 rounded-lg shadow-lg flex flex-col items-center hover:scale-105 transition-transform duration-300">
+                  <div className="grid justify-items-center md:flex lg:flex text-center  gap-5 hover:scale-105 transition-transform duration-300">
                     <img
                       src={rishi}
                       alt="Author- img"
                       loading="lazy"
-
                       className="w-16 h-16 rounded-full mb-4"
                     />
                     <div>
                       <h3 className="text-lg font-bold">Rishav Kumar</h3>
 
-                      <p className="text-sm text-gray-500">
+                      <p className="text-md text-gray-700 font-semibold">
                         Mern Stack Developer
                       </p>
                     </div>
                   </div>
 
-                  <p className="text-gray-700 text-center mt-4">
+                  <p className="text-gray-900 text-center mt-4">
                     I recently purchased a dining table and chairs from this
                     store, and I couldn't be happier with the experience.
                   </p>
                 </div>
-                <div className=" bg-white p-6 rounded-lg shadow-lg flex flex-col items-center w-80 min-w-[80%] md:min-w-[30%] transition-transform duration-300">
-                  <div className="flex items-center gap-10">
+                <div className=" bg-white p-6 rounded-lg shadow-lg flex flex-col items-center  hover:scale-105 transition-transform duration-300">
+                  <div className="grid justify-items-center md:flex lg:flex text-center  gap-5 hover:scale-105 transition-transform duration-300">
                     <img
                       src={pic}
                       alt="Author- img"
                       loading="lazy"
-
                       className="w-16 h-16 rounded-full mb-4"
                     />
                     <div>
                       <h3 className="text-lg font-bold">James Anderson</h3>
 
-                      <p className="text-sm text-gray-500">UI/UX Designer</p>
+                      <p className="text-md text-gray-700 font-semibold">
+                        UI/UX Designer
+                      </p>
                     </div>
                   </div>
 
-                  <p className="text-gray-700 text-center mt-4">
+                  <p className="text-gray-900 text-center mt-4">
                     I recently purchased a dining table and chairs from this
                     store, and I couldn't be happier with the experience.
                   </p>
@@ -398,39 +395,74 @@ const HeroSection = () => {
               </div>
 
               {/* Navigation Arrows */}
-              <div className="absolute top-1/2 -left-6 transform -translate-y-1/2">
+              {/* <div className="absolute top-1/2 -left-6 transform -translate-y-1/2">
                 <button className="p-2 bg-gray-200 rounded-full hover:bg-gray-300">
-                  {/* <FaArrowLeft /> */}
+                  <FaArrowLeft />
                 </button>
               </div>
               <div className="absolute top-1/2 -right-6 transform -translate-y-1/2">
                 <button className="p-2 bg-gray-200 rounded-full hover:bg-gray-300">
-                  {/* <FaArrowRight /> */}
+                  <FaArrowRight />
                 </button>
-              </div>
+              </div> */}
             </div>
           </div>
         </section>
 
         {/*Reviews  */}
         <section className="bg-gray-100 p-8  shadow-md  mx-auto">
-          <h3 className="text-blue-900 font-bold text-lg mb-4">REVIEWS</h3>
-          <h2 className="text-gray-900 text-2xl font-semibold mb-6">
+        <h2 className="text-xl text-blue-900 font-semibold mb-2">
+        REVIEWS</h2>
+          <h3 className="text-gray-900 text-3xl font-bold mb-6">
             What They Say About Us
-          </h2>
-          <div className=" grid md:grid-cols  sm:grid-cols-1 lg:flex gap-4 overflow-x-auto scrollbar-hide ">
-            <div className="bg-white  rounded-lg shadow-lg p-2 text-center w-80 min-w-[80%] md:min-w-[30%] transition-transform duration-300">
-              <div className=" bg-red-100  rounded-lg  flex items-center gap-4">
+          </h3>
+          <div className=" grid md:grid-cols-1  sm:grid-cols lg:flex gap-4 ">
+            <div className="bg-white  rounded-lg shadow-lg p-2 text-center ">
+            <div className="grid justify-items-center md:flex lg:flex text-center gap-5 mx-5 mt-5 hover:scale-50 transition-transform duration-200">
+
+
                 <img
                   src="/pic.png"
                   alt="User Avatar"
                   className="w-12 h-12 rounded-full mr-4"
                 />
-                <h4 className="text-gray-900 font-semibold">James Anderson</h4>
+                <h4 className="text-gray-900 font-bold">James Anderson</h4>
                 <p className="text-gray-500 text-sm">UI/UX Designer</p>
               </div>
 
-              <div className="bg-green-100 p-5 rounded-lg shadow-lg mt-5 ">
+              <div className="bg-gray-100 p-5 rounded-lg shadow-lg mt-5 hover:scale-95 transition-transform duration-300 cursor-pointer ">
+                <h3 className="text-gray-900 font-bold mt-4">
+                  Absolutely Thrilled with My Purchase!
+                </h3>
+                <p className="text-gray-600 text-sm mt-2 ">
+                  I recently purchased a dining table and chairs from this
+                  store, and I couldn't be happier with the experience. The
+                  quality of the furniture is outstanding—beautifully crafted
+                  and very sturdy. The delivery was prompt, and the assembly
+                  service was efficient and professional. The customer service
+                  team was incredibly helpful and answered all my questions with
+                  patience. I've already recommended this store to friends and
+                  family, and I will definitely be shopping here again. Highly
+                  recommended!
+                </p>
+              </div>
+            </div>
+
+            <div className="bg-white  rounded-lg shadow-lg p-2 text-center hover:scale-105 transition-transform duration-900">
+            <div className="grid justify-items-center md:flex lg:flex text-center gap-5 mx-5 mt-5 hover:scale-50 transition-transform duration-500">
+
+
+                <img
+                  src="/pic.png"
+                  alt="User Avatar"
+                  className="w-12 h-12 rounded-full mr-4"
+                />
+                <h4 className="text-gray-900 font-bold">James Anderson</h4>
+                <p className="text-gray-500 text-sm">UI/UX Designer</p>
+              </div>
+
+              <div className="bg-gray-100 p-5 rounded-lg shadow-lg mt-5 hover:scale-95 transition-transform duration-300 cursor-pointer ">
+
                 <h3 className="text-gray-900 font-bold mt-4">
                   Absolutely Thrilled with My Purchase!
                 </h3>
@@ -447,47 +479,20 @@ const HeroSection = () => {
                 </p>
               </div>
             </div>
+            <div className="bg-white  rounded-lg shadow-lg p-2 text-center">
+            <div className="grid justify-items-center md:flex lg:flex text-center gap-5 mx-5 mt-5 hover:scale-50 transition-transform duration-900">
 
-            <div className="bg-white  rounded-lg shadow-lg p-2 text-center w-80 min-w-[80%] md:min-w-[30%] transition-transform duration-300">
-              <div className=" bg-red-100  rounded-lg  flex items-center gap-4">
                 <img
                   src="/pic.png"
                   alt="User Avatar"
                   className="w-12 h-12 rounded-full mr-4"
                 />
-                <h4 className="text-gray-900 font-semibold">James Anderson</h4>
+                <h4 className="text-gray-900 font-bold">James Anderson</h4>
                 <p className="text-gray-500 text-sm">UI/UX Designer</p>
               </div>
 
-              <div className="bg-green-100 p-5 rounded-lg shadow-lg mt-5 ">
-                <h3 className="text-gray-900 font-bold mt-4">
-                  Absolutely Thrilled with My Purchase!
-                </h3>
-                <p className="text-gray-600 text-sm mt-2">
-                  I recently purchased a dining table and chairs from this
-                  store, and I couldn't be happier with the experience. The
-                  quality of the furniture is outstanding—beautifully crafted
-                  and very sturdy. The delivery was prompt, and the assembly
-                  service was efficient and professional. The customer service
-                  team was incredibly helpful and answered all my questions with
-                  patience. I've already recommended this store to friends and
-                  family, and I will definitely be shopping here again. Highly
-                  recommended!
-                </p>
-              </div>
-            </div>
-            <div className="bg-white  rounded-lg shadow-lg p-2 text-center w-80 min-w-[80%] md:min-w-[30%] transition-transform duration-300">
-              <div className=" bg-red-100  rounded-lg  flex items-center gap-4">
-                <img
-                  src="/pic.png"
-                  alt="User Avatar"
-                  className="w-12 h-12 rounded-full mr-4"
-                />
-                <h4 className="text-gray-900 font-semibold">James Anderson</h4>
-                <p className="text-gray-500 text-sm">UI/UX Designer</p>
-              </div>
+              <div className="bg-gray-100 p-5 rounded-lg shadow-lg mt-5 hover:scale-95 transition-transform duration-300 cursor-pointer ">
 
-              <div className="bg-green-100 p-5 rounded-lg shadow-lg mt-5 ">
                 <h3 className="text-gray-900 font-bold mt-4">
                   Absolutely Thrilled with My Purchase!
                 </h3>
